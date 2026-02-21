fb14dbf2e970e5f251e2435a7190ed5c6d96e6d1
celestialcharm <suryarock156169344@gmail.com>
2026-02-21 19:49:01 +0530

    yml file for daily run as well as on PR

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
